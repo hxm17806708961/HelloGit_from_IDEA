53fecb85abfa40f6da7bc4f2eebdd7b68911f52e
hxm <550085798>
2020-10-26 20:19:32 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/hxm/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.hxm.git;
+
+/**
+ * @author hxm
+ * @date 2020-10-26 20:05
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+
+        System.out.println("hello git !");
+    }
+}
